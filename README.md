ac3c61ec52f5d83d0d0e1eb8d3ff394d49ae909a
dvillarama <dvillarama@gmail.com>
2021-03-29 14:23:32 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # veggie-project
+
+## Tools
+- node v14.16.0
+
+## Installation
+`yarn`
+
+## Running Tests
+`yarn test`
--- tests/test.test.js
+++ tests/test.test.js
@@ -4,7 +4,6 @@ test('add 1 + 2 to equal 3', () => {
   expect(sum([1,2])).toBe(3);
 });
 
-
 test('add 1 + 2 + 3 to equal 3', () => {
   expect(sum([1,2,3])).toBe(6);
 });
